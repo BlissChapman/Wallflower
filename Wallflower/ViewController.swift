81e232b3400cfe6b7185cf4e0b749e0d32f4857c
Justin Loew <jloloew@gmail.com>
2015-11-22 06:00:04 -0600

    add pods

--- Wallflower/ViewController.swift
+++ Wallflower/ViewController.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
+import Alamofire
+import SwiftyJSON
+
+let WalgreensAPIKey = "ej1qRGcociFTwC3HFa00v3Wxra6gbwRJ"
 
 class ViewController: UIViewController {
 
@@ -20,6 +25,66 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func choosePhotoPressed(sender: AnyObject) {
+		
+	}
+	
+	func sendChosenPhoto(photo: UIImage) {
+		let data = UIImageJPEGRepresentation(photo, 1)!
+		
+		let request = NSMutableURLRequest(URL: NSURL(string: "http://wallflower.azurewebsites.net/upload")!)
+		request.HTTPMethod = "POST"
+		request.HTTPBody = data
+		
+		NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
+			guard error == nil else {
+				print(error!.localizedDescription)
+				return
+			}
+			guard let data = data else {
+				print("no data received in reply")
+				return
+			}
+			
+			let mosaicImage = UIImage(data: data)!
+			print("got image back from server")
+			
+			self.sendToWalgreens(mosaicImage)
+		}.resume()
+	}
+	
+	/// Send a mosaic image to Walgreens
+	func sendToWalgreens(mi: UIImage) {
+		
+	}
 
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+	func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+		defer {
+			picker.dismissViewControllerAnimated(true, completion: nil)
+		}
+		guard let photo = info[UIImagePickerControllerEditedImage] as? UIImage else {
+			return
+		}
+		sendChosenPhoto(photo)
+	}
+	
+	func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+		picker.dismissViewControllerAnimated(true, completion: nil)
+	}
+	
+	private func determinePermissionStatus(completion: (Bool) -> Void) {
+		let status = AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)
+		switch status {
+		case .Authorized:
+			completion(true)
+		case .Denied, .Restricted:
+			completion(false)
+		case .NotDetermined:
+			AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo, completionHandler: completion)
+		}
+	}
 }
 
